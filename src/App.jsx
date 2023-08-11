d54a3274f13efc7e8aa9a21174e75bd2be6cb7ec
adamnBlack <mc2933088@gmail.com>
2023-08-11 08:42:19 -0700

    fix the white pad

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import {
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/3D-project">
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
